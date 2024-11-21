51ebaf1a78c5c450fd1835b0ad2342976f1d87e7
Ruben <rubenovidalo@gmail.com>
2024-11-21 09:34:23 -0500

    module 3

---
+++ module_3_assignment/launch/gazebo_ackerman.launch.py
@@ -0,0 +1,47 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
+from launch.actions import ExecuteProcess
+
+def generate_launch_description():
+    pkg_path = get_package_share_directory('module_3_assignment')
+    urdf_file = os.path.join(pkg_path, 'urdf', 'ackerman_mobile.urdf')
+
+    return LaunchDescription([
+        # Node for publishing robot state
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[{"use_sim_time": True},
+                        {"publish_frequency": 100.0}
+                        ],
+            arguments=[urdf_file]
+        ),
+        # Node for publishing joint states
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            output='screen',
+            arguments=[urdf_file]
+        ),
+        # Gazebo simulator process
+        ExecuteProcess(
+            cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_factory.so'],
+            output='screen'
+        ),
+        # Node for spawning the robot in Gazebo
+        Node(
+            package='gazebo_ros',
+            executable='spawn_entity.py',
+            name='robot_spawner',
+            output='screen',
+            arguments=[
+                '-topic', '/robot_description',
+                '-entity', 'diff_drive_bot'
+            ]
+        )
+    ])
---
+++ module_3_assignment/task.md
@@ -0,0 +1,69 @@
+# Module 3 Assignment: URDF and Robot Creation in ROS 2
+
+## module_3_assignment package provided urdf models
+- Ackerman Mobile Robot
+- 3DOF Arm Robot
+
+## How to clone && build the project
+
+
+1. Clone the repository
+
+```sh
+git clone https://github.com/Combuster54/robotics_software_engineer.git
+```
+
+2. Go into the repo and change into assignments branch
+
+```sh
+src robotics_software_engineer/
+git switch assignments
+```
+
+3. Get back to your workspace and compile 
+
+```sh
+colcon build && source install/setup.bash
+```
+
+## Execute Task 1 && Task 2
+
+### Goals: Create a 3DOF Arm Robot
+
+1. Launch Arm Robot on Rviz
+
+```sh
+ros2 launch module_3_assignment rviz_3dof_arm.launch.py
+```
+
+2. Launch Arm Robot on Rviz
+
+```sh
+ros2 launch module_3_assignment gazebo_3dof_arm.launch.py
+```
+
+## Execute Task 3
+
+1. Launch Arm Robot + Diff Drive Robot on Rviz
+
+```sh
+ros2 launch module_3_assignment rviz_robot.launch.py
+```
+
+2. Launch Arm Robot + Diff Drive Robot on Gazebo
+
+```sh
+ros2 launch module_3_assignment gazebo_robot.launch.py
+```
+
+3. Launch Ackerman Robot on Rviz
+
+```sh
+ros2 launch module_3_assignment rviz_ackerman.launch.py
+```
+
+4. Launch Ackerman Robot on Gazebo
+
+```sh
+ros2 launch module_3_assignment gazebo_ackerman.launch.py
+```
